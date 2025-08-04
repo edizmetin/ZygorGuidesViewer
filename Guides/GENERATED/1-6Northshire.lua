e60e0df5c60c2606657704087789e51c94b790f3
SimonGaufreteau <simon.gaufreteau@gmail.com>
2025-08-04 14:32:48 +0200

    Manually add generated guides to autoload

---
+++ Guides/GENERATED/1-6Northshire.lua
@@ -0,0 +1,112 @@
+local ZygorGuidesViewer=ZygorGuidesViewer
+if not ZygorGuidesViewer then return end
+if UnitFactionGroup("player")~="Alliance" then return end
+ZygorGuidesViewer:RegisterGuide("GENERATED\\1-6Northshire",[[
+author TUGs
+next GENERATED\\1-9ElwynnForest
+startlevel 1
+step //1
+accept A Threat Within##783
+step //2
+turnin A Threat Within##783
+step //3
+accept Kobold Camp Cleanup##7
+step //4
+accept Eagan Peltskinner##5261
+step //5
+turnin Eagan Peltskinner##5261
+step //6
+accept Wolves Across the Border##33
+step //7
+Do [QC33- ][QC7]
+step //8
+turnin Wolves Across the Border##33
+step //9
+Sell your items at Brother Danil [G47.6, 41.6 Elwynn Forest].
+step //10
+[A Warlock,Mage] Buy 10 Water
+step //11
+turnin Kobold Camp Cleanup##7
+step //12
+accept Investigate Echo Ridge##15
+step //13
+accept Simple Letter##3100
+|only if Warrior
+step //14
+accept Consecrated Letter##3101
+|only if Paladin
+step //15
+accept Tainted Letter##3105
+|only if Warlock
+step //16
+accept Glyphic Letter##3104
+|only if Mage
+step //17
+accept Brotherhood of Thieves##18
+step //18
+accept The Stolen Tome##1598
+|only if Warlock
+step //19
+turnin Tainted Letter##3105
+|only if Warlock
+step //20
+Do: The Stolen Tome
+|only if Warlock
+step //21
+Do: Brotherhood of Thieves
+step //22
+turnin Brotherhood of Thieves##18
+step //23
+accept Milly Osworth##3903
+step //24
+accept Bounty on Garrick Padfoot##6
+step //25
+Sell your items at Brother Danil [G47.6, 41.6 Elwynn Forest].
+step //26
+turnin Consecrated Letter##3101
+|only if Paladin
+step //27
+turnin The Stolen Tome##1598
+|only if Warlock
+step //28
+turnin Glyphic Letter##3104
+|only if Mage
+step //29
+turnin Simple Letter##3100
+|only if Warrior
+step //30
+Get your level 4 spells at your trainer
+step //31
+Do: Investigate Echo Ridge
+step //32
+turnin Milly Osworth##3903
+step //33
+accept Milly's Harvest##3904
+step //34
+Do [QC3904- ][QC6]
+step //35
+Grind until level5
+|ding 5
+step //36
+use Hearthstone##6948
+step //37
+turnin Bounty on Garrick Padfoot##6
+step //38
+turnin Investigate Echo Ridge##15
+step //39
+accept Skirmish at Echo Ridge##21
+step //40
+Do: Skirmish at Echo Ridge
+step //41
+turnin Milly's Harvest##3904
+step //42
+accept Grape Manifest##3905
+step //43
+turnin Skirmish at Echo Ridge##21
+step //44
+accept Report to Goldshire##54
+step //45
+turnin Grape Manifest##3905
+step //46
+accept Rest and Relaxation##2158
+]]
---
+++ Guides/GENERATED/6-9ElwynnForest.lua
@@ -0,0 +1,181 @@
+local ZygorGuidesViewer=ZygorGuidesViewer
+if not ZygorGuidesViewer then return end
+if UnitFactionGroup("player")~="Alliance" then return end
+ZygorGuidesViewer:RegisterGuide("GENERATED\\6-9ElwynnForest",[[
+author TUGs
+startlevel 6
+step //1
+accept Kobold Candles##60
+step //2
+home Elwynn Forest
+step //3
+turnin Rest and Relaxation##2158
+step //4
+turnin Report to Goldshire##54
+step //5
+accept The Fargodeep Mine##62
+step //6
+accept Gold Dust Exchange##47
+step //7
+Kill all Boars(especially good on the fields you will go to) for 4x Chunk of Boar Meat
+step //8
+accept Young Lovers##106
+step //9
+Well be crossing the Kobold mine 4 times now->Kill 2-3 Kobolds each time you do, to get some quest items already.
+step //10
+accept Princess Must Die!##88
+step //11
+accept Lost Necklace##85
+step //12
+turnin Young Lovers##106
+step //13
+accept Speak with Gramma##111
+step //14
+turnin Speak with Gramma##111
+step //15
+accept Note to William##107
+step //16
+turnin Lost Necklace##85
+step //17
+accept Pie for Billy##86
+step //18
+turnin Pie for Billy##86
+step //19
+accept Back to Billy##84
+step //20
+turnin Back to Billy##84
+step //21
+accept Goldtooth##87
+step //22
+Do [QC62- ][QC47- ][QC60- ][QC87]
+step //23
+Die and rezz at Ghosthealer
+step //24
+Turn on your Aura.
+|only if Paladin
+step //25
+turnin The Fargodeep Mine##62
+step //26
+accept The Jasperlode Mine##76
+step //27
+Run to 41.5,65.8 |goto Elwynn Forest 41.5,65.8
+|only if Warrior,Paladin
+step //28
+Get your level 6 spells at your trainer
+|only if Warrior,Paladin
+step //29
+turnin Gold Dust Exchange##47
+step //30
+accept A Fishy Peril##40
+step //31
+turnin A Fishy Peril##40
+step //32
+accept Further Concerns##35
+step //33
+turnin Note to William##107
+step //34
+accept Collecting Kelp##112
+step //35
+turnin Kobold Candles##60
+step //36
+accept Shipment to Stormwind##61
+step //37
+Get your level 6 spells at your trainer
+|only if Warlock,Mage,Priest,Rogue
+step //38
+[A Paladin,Mage,Warlock] Buy 20 Milk and as many 6 Slot Bags as you can.
+|only if Paladin,Mage,Warlock
+step //39
+[A Warrior,Rogue] Buy 20 Food(lvl5) and as many 6 Slot Bags as you can.
+|only if Warrior,Rogue
+step //40
+Do: Collecting Kelp
+step //41
+Do: The Jasperlode Mine
+step //42
+Die on the Campfire nearby[G64.5,56.6Elwynn Forest] and rezz at Ghosthealer.
+step //43
+Turn on your Aura.
+|only if Paladin
+step //44
+Repair your gear and sell grey items at Rallic Finn [G83.3, 66.1Elwynn Forest]
+step //45
+accept A Bundle of Trouble##5545
+step //46
+turnin Further Concerns##35
+step //47
+accept Find the Lost Guards##37
+step //48
+accept Protect the Frontier##52
+step //49
+turnin Find the Lost Guards##37
+step //50
+accept Discover Rolf's Fate##45
+step //51
+Do [QC5545- ][QC52]
+step //52
+turnin Discover Rolf's Fate##45
+step //53
+accept Report to Thomas##71
+step //54
+turnin A Bundle of Trouble##5545
+step //55
+turnin Protect the Frontier##52
+step //56
+turnin Report to Thomas##71
+step //57
+accept Deliver Thomas' Report##39
+step //58
+Do (Optional): Princess Must Die!
+step //59
+This can be done solo by most classes by so called "Fence hopping". You Attack/Frostbolt/Shadowbolt, you jump the fence and repeat. Its pretty hard and you can learn it by killing defias/bears/boars first. Doing it vs Princes and her 2 escorts is even harder,MOST PEOPLE wont be able to do this!!! Otherwise: skip it as mentioned.
+step //60
+Run to [G84.4, 76.5Elwynn Forest] for Ridgepoint Tower exploring experience.
+step //61
+Run to [G9.8, 75.6Redridge Mountains] and die to the spiders and rezz up in Redridge.
+step //62
+Turn on your Aura.
+|only if Paladin
+step //63
+Run to [G25.1,57.0Redridge Mountains] for Lake Everstill exploring experience.
+step //64
+fpath Redridge Mountains |goto Redridge Mountains 30.6, 59.4
+step //65
+use Hearthstone##6948
+step //66
+Get your level 8 spells at your trainer
+|only if Mage,Warlock,Priest,Rogue
+step //67
+turnin Collecting Kelp##112
+step //68
+accept The Escape##114
+step //69
+turnin The Jasperlode Mine##76
+step //70
+turnin Deliver Thomas' Report##39
+step //71
+The next quest "Elmore's Task" is shown in Redridge Mountains. Just enter the Blacksmith in Goldshire. It's there instead.
+step //72
+accept Elmore's Task##1097
+step //73
+Get your level 8 spells at your trainer
+|only if Paladin,Warrior
+step //74
+turnin The Escape##114
+step //75
+turnin Princess Must Die!##88
+step //76
+turnin Goldtooth##87
+step //77
+turnin Shipment to Stormwind##61
+step //78
+turnin Elmore's Task##1097
+step //79
+accept Stormpike's Delivery##353
+step //80
+Take the Tram to Ironforge
+step //81
+fpath Ironforge |goto Ironforge 55.5,47.8
+step //82
+home Ironforge
+]]
